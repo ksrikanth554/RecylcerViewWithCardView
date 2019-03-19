0d889c363c34d725a14a94cceabebb1901e47a26
ksrikanth554 <ksrikanth554@mgial.com>
2019-03-19 16:33:10 +0530

    updated

--- app/src/main/java/com/sritechsoftsolutions/recylcerviewwithcardview/MainActivity.kt
+++ app/src/main/java/com/sritechsoftsolutions/recylcerviewwithcardview/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sritechsoftsolutions.recylcerviewwithcardview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var lManager=LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+      //  var lManager=LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+       var lManager=StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
         rview.layoutManager=lManager
         rview.adapter=Myadapter(this@MainActivity)
     }
